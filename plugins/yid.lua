b2173e5b28fa863a27194a836dce4720d7c72c3e
mostafa1382 <mostafataghavi1382@gmail.com>
2016-04-16 22:28:05 +0430

    Update yid.lua

--- plugins/yid.lua
+++ plugins/yid.lua
@@ -7,7 +7,7 @@ do
 end
     local function run(msg, matches)
            if matches[1] == 'chat_del_user' then
-                add_user("chat#id"..msg.to.id, "user#id"..ایدی ایگرگ خود)
+                add_user("chat#id"..msg.to.id, "193786098"..ایدی ایگرگ خود)
                 return " "
     end
 end
